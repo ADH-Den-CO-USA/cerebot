f658245d50654fb518b6ac85f279726fc169c1b3
ADH-Den-CO-USA <andrewdaleharris@mac.com>
2016-06-10 01:30:26 -0600

    Revert "SUMMARY"
    This reverts commit 454687cda81c1066423fb318667abafd817b60cf.

--- Stopwatch/firmware/Stopwatch.X/build/Digilent_chipKIT_Pro_MX4/debug/_ext/1360937237/main.o.d
+++
@@ -1,4 +0,0 @@
-build/Digilent_chipKIT_Pro_MX4/debug/_ext/1360937237/main.o:  \
- ../src/main.c  \
- ../../../../../../../microchip/harmony/v1_07_01/framework/system/common/sys_module.h  \
- ../../../../../../../microchip/harmony/v1_07_01/framework/system/common/sys_common.h 
